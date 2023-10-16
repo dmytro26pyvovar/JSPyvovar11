99458ddcb885a2ee62c54ea093f809136ef57eff
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-10-16 12:13:32 +0200

    +

---
+++ src/index.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+import Notiflix from "notiflix";
+
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreButton = document.querySelector('.load-more');
+
+let page = 1;
+let searchQuery = '';
+
+async function fetchImages(query, pageNum) {
+    try {
+        const apiKey = '40077620-b33d7d6f744e065ea0d98c96c';
+        const perPage = 40;
+        const response = await axios.get('https://pixabay.com/api/', {
+            params: {
+                key: apiKey,
+                q: query,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: true,
+                page: pageNum,
+                per_page: perPage,
+            },
+        });
+        
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching images:', error);
+        return null;
+    }
+};
+
+function renderImageCards(images) {
+    images.forEach((image) => {
+        const card = document.createElement('div');
+        card.className = 'photo-card';
+        card.innerHTML = `
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+                <p class="info-item"><b>Views:</b> ${image.views}</p>
+                <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+                <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+            </div>
+        `;
+        gallery.appendChild(card);
+    });
+};
+
+searchForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    page = 1;
+    searchQuery = searchForm.searchQuery.value;
+    gallery.innerHTML = '';
+    loadMoreButton.style.display = 'none';
+    const imagesData = await fetchImages(searchQuery, page);
+    if (imagesData && imagesData.hits.length > 0) {
+        renderImageCards(imagesData.hits);
+        loadMoreButton.style.display = 'block';
+    } else {
+        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    }
+});
+
+loadMoreButton.addEventListener('click', async () => {
+    page++;
+    const imagesData = await fetchImages(searchQuery, page);
+    if (imagesData && imagesData.hits.length > 0) {
+        renderImageCards(imagesData.hits);
+    } else {
+        loadMoreButton.style.display = 'none';
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+});
